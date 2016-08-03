191d9a8dc025d6adf3551c419ee1e10cb653ad65
edkoma <edward.zc.wang@gmail.com>
2016-08-03 12:21:01 -0400

    test sync

--- sweetalerttest.php
+++ sweetalerttest.php
@@ -2,5 +2,5 @@
 <link rel="stylesheet" type="text/css" href="sweetalert/dist/sweetalert.css">
 <input type="button" value="click me you fuck!" onClick="swal('Got you mofo!');">
 <?php
-	echo '<script> swal("Here a message!"); </script>';  
+  
 ?>
