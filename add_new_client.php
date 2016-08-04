3d50084ddee55bebed3c01ac576d633c3f34088f
Stephen Ayre <stephen.ayre@aol.com>
2016-08-04 16:58:56 -0400

    hh

--- add_new_client.php
+++ add_new_client.php
@@ -2,21 +2,16 @@
 
 require ("connection.php");
 
-$full_name = $_POST['full_name'];
-$address_line_1 = $_POST['address_line_1'];
-$business = $_POST['business'];
-$phone = $_POST['phone'];
-$cell_phone = $_POST['cell_phone'];
-$city = $_POST['city'];
-$state = $_POST['state'];
-$zipcode = $_POST['zipcode'];
-$source = $_POST['source'];
-$second_contact = $_POST['second_contact'];
-$rep = $_POST['rep'];
-$title = $_POST['title'];
-$fax = $_POST['fax'];
-$web_address = $_POST['web_address'];
+$client_name = $_POST['client_name'];
+$client_add = $_POST['client_add'];
+$contact_name = $_POST['contact_name'];
+$contact_phone = $_POST['contact_phone'];
+$contact_email = $_POST['contact_email'];
+$category = $_POST['category'];
+$sec1 = $_POST['sec1'];
+$website = $_POST['website'];
 $notes = $_POST['notes'];
+$title = $_POST['title'];
 
 session_start();
 
@@ -28,24 +23,22 @@ $_SESSION['date'] = $today;
 $job = "added new client";
 $today2 = date("Y-m-d");
 
-$check=mysqli_query($conn,"select * from sales where (full_name='$full_name' and address_line_1 ='$address_line_1') or full_name = '$full_name'");
+$check=mysqli_query($conn,"SELECT * FROM client_info where (client_name='$client_name' and client_add='$client_add') or client_name = '$client_name'");
     $checkrows=mysqli_num_rows($check);
 
     if ($checkrows>0){
 	   echo '<script>alert("This record has already been added.");
-	   window.location.href = "clients.php";
-	   </script>';  
+	   window.location.href = "clients.php";</script>';
    }
-   
-   else{
-	   $sql = "INSERT INTO sales (full_name,address_line_1,business,phone,cell_phone,city,state,zipcode,source,second_contact,rep,title,fax,web_address,notes,type,date_added) VALUES ('$full_name', '$address_line_1', '$business', '$phone','$cell_phone','$city','$state','$zipcode','$source','$second_contact','$rep', '$title', '$fax', '$web_address', '$notes', 'Client', '$today2')";
-		$result = $conn->query($sql) or die('error');
 
-		$sql6 = "INSERT INTO timestamp (user,time,job, a_p) VALUES ('$user_name', '$today','$job', '$a_p')";
-		$result7 = $conn->query($sql6) or die('Error querying database 5.');
-   }
-   
+$sql = "INSERT INTO client_info (client_name,client_add,contact_name,contact_phone,contact_email,sec1, website,notes,category,title,date_added) VALUES ('$client_name', '$client_add', '$contact_name', '$contact_phone','$contact_email','$sec1','$website','$notes','$category','$title','$today2')";
+$result = $conn->query($sql) or die('error');
+
+$sql6 = "INSERT INTO timestamp (user,time,job, a_p) VALUES ('$user_name', '$today','$job', '$a_p')";
+$result7 = $conn->query($sql6) or die('Error querying database 5.');
+ 
 $conn->close();
+
 header("location: clients.php");
 exit();
 
