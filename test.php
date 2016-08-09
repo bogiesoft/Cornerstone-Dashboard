873696e238cce1d8d831dd7e617d25288fa2631f
edkoma <edward.zc.wang@gmail.com>
2016-08-09 09:11:16 -0400

    some updates

---
+++ test.php
@@ -0,0 +1,12 @@
+<script src="jquery.js"></script>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script>
+ $(function(){
+  $("#test").click(function(){
+       alert("ahhhh");
+
+});
+});
+</script>
+<button id='test' >delete</button>
+
