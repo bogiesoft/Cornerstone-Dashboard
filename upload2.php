c536464a8fc0994fe169e88875118df1f6626e08
edkoma <edward.zc.wang@gmail.com>
2016-08-16 16:55:42 -0400

    frustrated...

--- upload2.php
+++ upload2.php
@@ -29,7 +29,7 @@ if ($uploadOk == 0) {
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname= "crst_dashboard";
+$dbname= "test";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -41,9 +41,9 @@ if ($conn->connect_error) {
    FIELDS TERMINATED BY '\t'
 LINES TERMINATED BY '\r'
 IGNORE 1 LINES
-(var1, var2, var3, var4, var5, var6, var7, var8, var9, var10, var11, var12, var13, var14, @var15, var16, @var17, var18, var19, var20, var21, var22, var23, var24, var25, var26, var27, var28, var29, var30, var31, var32, var33, var34, var35, var36, var37, var38, var39, var40, var41) 
-SET call_back_date = STR_TO_DATE(@var15, '%m/%d/%Y'),     
-	date_added = STR_TO_DATE(@var17, '%m/%d/%Y')
+(@var1, @var2) 
+SET begin = STR_TO_DATE(@var1, '%m/%d/%Y'),     
+end = STR_TO_DATE(@var2, '%m/%d/%Y')
 ";
 
     $result = $conn->query($sql) or die('Error querying database.');
