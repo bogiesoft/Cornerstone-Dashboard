110dc40fdfd0095ed630ae0d89efe0422bba710d
edkoma <edward.zc.wang@gmail.com>
2016-08-21 12:41:40 -0400

    fixed W_M select table in edit and add job ticket page

--- job_ticket.php
+++ job_ticket.php
@@ -4,8 +4,12 @@ require ("header.php");
 
 
 <script>
-var W_Mcount=1;
+	var id_of_row;
+	var number_of_rows;
+
 	$(function() {
+		id_of_row=parseInt($( "tr:last" ).attr('id'));
+		number_of_rows=id_of_row;
     $(document).on('change', '.vendors',function(){
     	var id=$(this).parent().parent().attr('id');
     	getMaterials(id);
@@ -29,7 +33,6 @@ function getMaterialsID(row_id){
         type: 'post',
         data:{vendor:vendor,material:material,type:type},
         success: function(data){
-        	alert(data);
         	var result=jQuery.parseJSON(data);
         	$.each(result,function( index, value ) {
 				$("#checkbox"+row_id).attr("value", value);
@@ -39,16 +42,17 @@ function getMaterialsID(row_id){
 
 };
 function addWeights_Measures(){
-	if(W_Mcount<20){
-		W_Mcount=W_Mcount+1;
-		$("#W_MTable").append(	"<tr id='"+W_Mcount+"'><td >			<input type='checkbox' id='checkbox"+W_Mcount+"'checked name='wm[]' value=''>		</td>		<td>			<select class='vendors' id='vendors"+W_Mcount+"' name='vendor' style='width:220px;'>				<option value=''>Select</option>			</select>		</td>		<td>			<select class='materials' id='materials"+W_Mcount+"' name='material' style='width:220px;'>				<option value=''>Select</option>			</select>		</td>		<td>			<select class='types' id='types"+W_Mcount+"' name='vendor' style='width:220px;'>				<option value=''>Select</option>			</select>		</td> <td><img src = 'images/x_button.png' width = '25' height = '25' onclick = removeWeights_Measures('#" + W_Mcount + "')></td>	</tr>");
-		getVendors(W_Mcount);
+	if(number_of_rows<20){
+		number_of_rows=number_of_rows+1;
+		id_of_row=id_of_row+1;
+		$("#W_M_tbody").append(	"<tr id='"+id_of_row+"'><td >			<input type='checkbox' id='checkbox"+id_of_row+"'checked name='wm[]' value=''>		</td>		<td>			<select class='vendors' id='vendors"+id_of_row+"' name='vendor' style='width:220px;'>				<option value=''>Select</option>			</select>		</td>		<td>			<select class='materials' id='materials"+id_of_row+"' name='material' style='width:220px;'>				<option value=''>Select</option>			</select>		</td>		<td>			<select class='types' id='types"+id_of_row+"' name='vendor' style='width:220px;'>				<option value=''>Select</option>			</select>		</td> <td><img src = 'images/x_button.png' width = '25' height = '25' onclick = removeWeights_Measures('#" + id_of_row + "')></td>	</tr>");
+		getVendors(id_of_row);
 
 	}
 };
 function removeWeights_Measures(row_id){
 	$(row_id).remove();
-	W_Mcount--;
+	number_of_rows--;
 };
 function getVendors(row_id)
 {
@@ -394,9 +398,12 @@ function getTypes(row_id)
 					<label>Weights and Measures</label>
 					<a class="pull-right" onclick = 'addWeights_Measures()'>Add Weights and Measures</a>
 					<table id="W_MTable" border="1" cellpadding="1" cellspacing="1" style='text-align: center; vertical-align: middle;'>
+						<thead>
 						<tr>
 					        <th>Select</th><th>Vendor</th><th>Material</th><th>type</th><th>Delete</th>
 					    </tr>
+					    </thead>
+					    <tbody id="W_M_tbody">
 						<?php
 						$result = mysqli_query($conn, "SELECT * FROM materials ORDER BY vendor");
 						while($row = $result->fetch_assoc()){
@@ -423,7 +430,7 @@ function getTypes(row_id)
 								    </tr>";
 								    }
 						?>
-
+						</tbody>
 					</table>
 					</div>
 					<div class="tabinner-detail">
