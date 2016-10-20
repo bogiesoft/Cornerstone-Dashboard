cbfc4c4ff3854ca1bffc7d15a566e5e793ce5acc
amitozdeol <amitozdeol@gmail.com>
2016-10-20 10:13:38 -0400

    New upload page
    uploadform_database shows seperate columns

--- uploadForm.php
+++ uploadForm.php
@@ -38,6 +38,7 @@ var CodeVersion = '3.0.8';
     <script type="text/javascript">
         $(function () {
             $("#upload").bind("click", function () {
+
                 var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
                 if (regex.test($("#fileUpload").val().toLowerCase())) {
                     if (typeof (FileReader) != "undefined") {
@@ -45,26 +46,28 @@ var CodeVersion = '3.0.8';
                         reader.onload = function (e) {
                             var table = $("<table border='1' cellspacing='1' cellpadding='1'/>");
                             var rows = e.target.result.split("\n");
-                            
+
                                 var row = $("<tr>");
                                 var cells = rows[0].split(",");
+                                //going through each cell name in CSV
                                 for (var j = 0; j < cells.length; j++) {
                                     var cell = $('<tr td>');
                                     cell.html(cells[j]);
                                     row.append(cell);
-									
+
 									//add select options
 									var selects = document.getElementsByClassName("import_dropdown");
-		
+
 									for(var i = 0; i < selects.length; i++){
 										var option = document.createElement("option");
 										option.text = cells[j];
 										option.value = cells[j];
+										option.name = cells[j];
 										selects[i].add(option);
 									}
                                 }
                                 table.append(row);
-                            
+
                             $("#dvCSV").html('');
                             $("#dvCSV").append(table);
 							$('.selected').removeClass('selected');
@@ -74,96 +77,66 @@ var CodeVersion = '3.0.8';
                     } else {
                         alert("This browser does not support HTML5.");
                     }
+
                 } else {
                     alert("Please upload a valid CSV file.");
                 }
             });
-			
-			    $("#display").click(function() {                
 
-					$.ajax({    //create an ajax request to load_page.php
-						type: "GET",
-						url: "uploadForm_database.php",             
-						dataType: "html",   //expect html to be returned                
-						success: function(response){                    
-							$("#responsecontainer").html(response); 
-							//alert(response);
-						}
-
-					});
-				});
         });
     </script>
-    <input type="file" id="fileUpload" />
-    <input type="button" id="upload" value="Upload" />
+    <input type="file" id="fileUpload" name = "fileUpload" form = "importForm"/>
+    <input type="button" id="upload" value="Upload" onclick="this.disabled='disabled'"/>
     <hr />
 	<fieldset>
 		<div class="widget-corner-upper"></div>
 			<div id="dvCSV">
 			</div>
-			<!--for database headers-->
-			
-			<table border="1" align="center">
-				<tr>
-					<td> <input type="button" id="display" value="Display All Data" /> </td>
-				</tr>
-			</table>
-			<div id = "responsecontainer" style="height:100px; overflow:auto;">
-			</div>
-			<!--for database headers-->
 		<div class="widget-corner-lower"></div>
 	</fieldset>
 <!--import complete-->
 
 </form>
-		
 
-		
+
+
 	<!--map the fields with tha database fields-->
   <div id="set_map">
 	<fieldset>
       <div class="widget-corner-upper"></div>
-      <legend>Map Fields</legend>
-
-     <div id="map-actions">
-         <button class="small" id="address-add">+ Address</button>
-         <button class="small" id="contribution-add">+ Contribution</button>
-         <button class="small" id="custom-add">+ Custom</button>
-         <button class="small" id="campaign-add">+ Campaign</button>
-     </div>
 
       <div id="map">
           <div id="data-fields"></div>
       <?php
-require("connection.php");
+		require("connection.php");
 
-$result = mysqli_query($conn,"SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'sales'");
+		$result = mysqli_query($conn,"SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'sales'");
 
-echo "<table id = 'import_table' border='1' >
-<tr>
-<td align=center> <b>Data Field</b></td><td>File Field</td>";
-if($result -> num_rows>0){
-while($data = $result->fetch_assoc())
-{   
-    echo "<tr>";
-    echo "<td align=center>$data[COLUMN_NAME]</td><td><select class = 'import_dropdown'><option value = 'none' style = 'display: none'>Choose one provided</option></select></td>";
-    echo "</tr>";
-}
-}else 
-	echo "no result";
-echo "</table>";
-?>
+		echo "<table id = 'import_table' border='1' >
+		<tr>
+			<td align=center> <b>Data Field</b></td><td>File Field</td>";
+		if($result -> num_rows>0){
+			while($data = $result->fetch_assoc()){
+	    		echo "<tr>";
+	    		echo "<td align=center>$data[COLUMN_NAME]</td><td><select class = 'import_dropdown' name= 'select1' form = 'importForm' ><option value = 'none' style = 'display: none'>Choose one provided</option></select></td>";
+	    		echo "</tr>";
+			}
+		}else
+			echo "no result";
+		echo "</table>";
+	?>
 </div>
 
-      <h4>File Fields</h4>
-      <div id="file-fields"><div class="list">    <div class="list-title"></div>    <ul><li>First Name</li><li>Last Name</li><li>Age</li></ul></div></div>
 
       <div class="widget-corner-lower"></div>
     </fieldset>
 
-    <div id="instance-actions">
-      <button id="import-accept">Import</button>
-    </div>
+	<!--import button-->
+	<form id = "importForm" action = "uploadForm_database.php" method = "POST" enctype="multipart/form-data">
+    	<div id="instance-actions">
+    	  <button id="import-accept">Import</button>
+    	</div>
+    </form>
 
    </div><!-- #set_map -->
    <div id="upload_file" class="hide">
--- uploadForm_database.php
+++ uploadForm_database.php
@@ -1,19 +1,54 @@
 <?php
 require("connection.php");
 
-$result = mysqli_query($conn,"SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'sales'");
 
-echo "<table border='1' >
-<tr>
-<td align=center> <b>Data Field</b></td>";
-if($result -> num_rows>0){
-while($data = $result->fetch_assoc())
-{   
-    echo "<tr>";
-    echo "<td align=center>$data[COLUMN_NAME]</td>";
-    echo "</tr>";
+$array = file($_FILES['fileUpload']["tmp_name"]);
+echo "<table border=1>";
+for ($j=0; $j < count($array); $j++) {
+  # code...
+   echo "<tr>";
+  $cell = explode(',',$array[$j]);
+  if ($cell[$j]==country) {
+    # code...
+  }
+  for ($i=0; $i <=2; $i++) {
+    # code..
+    echo "<td>".$cell[$i]."</td>";
+
+  }
+   echo "</tr>";
+
 }
-}else 
-	echo "no result";
-echo "</table>";
+
+//each row
+// for ($i=0; $i < count($array); $i++) {
+//
+//   $cell = split(",", $array[$i]);
+//   //each cell in a row
+//   for ($j=0; $j <count($cell) ; $j++) {
+//      echo "<td>".$cell[$j]."</td>";
+//   }
+//   echo "</tr>";
+// }
+// echo "</table";
+
+
+
+// $selected = $_POST['select1'];
+// echo "Master title".$file;
+// $result = mysqli_query($conn,"SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'sales'");
+//
+// echo "<table border='1' >
+// <tr>
+// <td align=center> <b>Data Field</b></td>";
+// if($result -> num_rows>0){
+// while($data = $result->fetch_assoc())
+// {
+//     echo "<tr>";
+//     echo "<td align=center>$data[COLUMN_NAME]</td>";
+//     echo "</tr>";
+// }
+// }else
+// 	echo "no result";
+// echo "</table>";
 ?>
