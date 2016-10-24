f6c9c08ae9ed814144d535b46f1297b3987b28a3
amitozdeol <amitozdeol@gmail.com>
2016-10-24 12:04:23 -0400

    General Bug fix
    Advance search in CRM Tab

--- sales.php
+++ sales.php
@@ -45,17 +45,154 @@ require ("header.php");
 		<div><a href="uploadForm.php">upload</a></div>
 	</div>
 	<div class="dashboard-detail">
-
-		
-<div class="search-cont">
-	<div class="searchcont-detail">
-		<div class="search-boxleft">
-			<label>Quick Search</label>
-			<input id="Jobsearch" name="frmSearch" type="text" placeholder="Search for a specific job">
+		<div class="newcontacts-tabs">
+			<ul class="nav nav-tabs" role="tablist">
+				<li><a href="#" class="tablinks" onclick="changeTab(event, 'internal')">Internal</a></li>
+				<li><a href="#" class="tablinks" onclick="changeTab(event, 'CRM')">CRM</a></li>
+			</ul>
+			<div class="contacts-title">
+			</div>
 		</div>
+		<div id="CRM" class="tab-content">
+			<div class="search-cont">
+				<div class="searchcont-detail">
+					<div class="search-boxleft">
+						<form id = "search_form" action="vendor_search.php" method="post" >
+							<label>Quick Search</label>
+							<input id="search" name="frmSearch" type="text" placeholder="Search for a specific client">
+						</form>
+					</div>
+					<div class="contacts-title">
+						<a id = 'advanced_search_button' class="pull-right" href="#" class="add_button" onclick = 'addField()'>Advanced Search</a>
+						<a id = 'show_saved_search' class="pull-right" class="add_button" onclick = 'showSavedSearch()' href = "#" style = "background: #ff5c33">Show Saved Search</a>
+						<form class = 'advanced_search_area' action = 'advanced_search_CRM.php' method = 'post'>
+							<input id = 'advanced_search_submit' name = 'advanced_search_submit' style = 'display: none; margin-right: 5%; margin-bottom: 5%; background-color: #000000; color: #ffffff' type = 'submit' value = "Search">
+							<input id = 'advanced_search_and_save' name = 'advanced_search_and_save' style = 'display: none; margin-bottom: 5%; background-color: #000000; color: #ffffff' type = 'submit' value = "Search and Save">
+							<input id = 'advanced_save' name = 'advanced_save' style = 'display: none; margin-bottom: 5%; margin-left: 5%; background-color: #000000; color: #ffffff' type = 'submit' value = "Save">
+							<input id = 'advanced_search_name' name = 'advanced_search_name' style = 'display: none; width: 200px; margin-left: 3%; margin-bottom: 3%' type = 'text' placeholder = 'Enter Saved Search Name'>
+						</form>
+					</div>
+					<div id="saved_search_div">
+						<table id="saved_search_table" style = 'display: none'>
+							<tbody>
+								<?php
+								$result = mysqli_query($conn, "SELECT * FROM saved_search WHERE table_type = 'CRM' ORDER BY search_date DESC LIMIT 10");
+								if (mysqli_num_rows($result) > 0) {
+							    // output data of each row
+									while($row = $result->fetch_assoc()) {
+										$search_id = $row["search_id"];
+										$field1=$row["field1"];
+										$value1=$row["value1"];
+										$field2=$row["field2"];
+										$value2=$row["value2"];
+										$field3=$row["field3"];
+										$value3=$row["value3"];
+										echo "<tr id = 'row" . $search_id . "'><td class='data-cell'><a href = 'advanced_search_CRM.php?field1=$field1&value1=$value1&field2=$field2&value2=$value2&field3=$field3&value3=$value3&search_id=$search_id'>". $row["search_name"]."</a></td><td><button id = '" . $search_id . "'><img src = 'images/x_button.png' width = '25' height = '25'></button></tr>";
+									}
+								}
+								else {
+									echo "0 Saved Searches";
+								}
+								?>
+							</tbody>
+						</table>
+					</div>
+
+				</div><br>
+<?php
+
+$result = mysqli_query($conn, "SELECT * FROM sales");
+
+echo " <div class='allcontacts-table'><table border='0' cellspacing='0' cellpadding='0' class='table-bordered allcontacts-table' >"; // start a table tag in the HTML
+echo "<tbody>";
+echo "<tr valign='top'><td colspan='2'><table id = 'crm_table' border='0' cellspacing='0' cellpadding='0' class='table-striped main-table contacts-list'><thead><tr valign='top' class='contact-headers'>
+<th class='maintable-thtwo data-header' data-name='Mark' data-index='0'>Mark</th>
+<th class='maintable-thtwo data-header' data-name='job_id' data-index='1'>Client Name</th>
+<th class='maintable-thtwo data-header' data-name='client_name' data-index='2'>Business</th>
+<th class='maintable-thtwo data-header' data-name='client_name' data-index='3'>Address</th>
+<th class='maintable-thtwo data-header' data-name='estimate_number' data-index='4'>City</th>
+<th class='maintable-thtwo data-header' data-name='estimate_number' data-index='5'>State</th>
+<th class='maintable-thtwo data-header' data-name='project_name' data-index='6'>Zip Code</th>
+<th class='maintable-thtwo data-header' data-name='records_total' data-index='7'>Call Back Date</th>
+<th class='maintable-thtwo data-header' data-name='records_total' data-index='8'>Priority</th>
+<th class='maintable-thtwo data-header' data-name='records_total' data-index='9'>Title</th>
+<th class='maintable-thtwo data-header' data-name='due_date' data-index='10'>Phone</th>
+<th class='maintable-thtwo data-header' data-name='records_total' data-index='11'>Website</th>
+<th class='maintable-thtwo data-header' data-name='records_total' data-index='12'>Email</th>
+<th class='maintable-thtwo data-header' data-name='records_total' data-index='13'>Vertical 1</th>
+<th class='maintable-thtwo data-header' data-name='records_total' data-index='14'>Vertical 2</th>
+<th class='maintable-thtwo data-header' data-name='records_total' data-index='15'>Vertical 3</th>
+</tr></thead><tbody>";
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+	/*	$website = $row['web_address'];
+		$email = $row['email1'];
+
+		if(strlen($website) >= 15){
+			$website = substr($website, 0, 15) . "<br>" . "...";
+		}
+		if(strlen($email) >= 15){
+			$email = substr($email, 0, 15) . "<br>" . "...";s
+		}
+*/
+		$foo=array();
+		array_push($foo, $row['full_name']);
+		array_push($foo, $row['address_line_1']);
+		$str = serialize($foo);
+		$stren = urlencode($str);
+		echo "<tr><td class = 'data-cell'><input type = 'checkbox' name = 'Mark[]'/></td>
+		<td class='data-cell'><a href = 'edit_client.php?client_info=$stren'>" .$row["full_name"]."</a></td>
+		<td class='data-cell'>".  $row["business"]."</td>
+		<td class='data-cell'>".  $row["address_line_1"]."</td>
+		<td class='data-cell'>" . $row["city"] . "</td>
+		<td class='data-cell'>" . $row["state"] . "</td>
+		<td class='data-cell'>". $row["zipcode"]. "</td>
+		<td class='data-cell'>". $row["call_back_date"]."</td>
+		<td class='data-cell'>". $row["priority"]."</td>
+		<td class='data-cell'>". $row["title"]."</td>
+		<td class='data-cell'>". $row["phone"]. "</td>
+		<td class='data-cell'>". $row["web_address"]."</td>
+		<td class='data-cell'>". $row["email1"]."</td>
+		<td class='data-cell'>". $row["vertical1"]."</td>
+		<td class='data-cell'>". $row["vertical2"]."</td>
+		<td class='data-cell'>". $row["vertical3"]."</td></tr>";
+    }
+	echo "</tbody></table></td></tr></tbody></table></div>";
+} else {
+    echo "0 results";
+}
+?>
+<div class="allcontacts-breadcrumbs">
+	<div class="allcontacts-breadcrumbsleft pull-left page-control">
+		<nav>
+			<ul class="pagination" id = "pag">
+				<li id = 'prev_button' class = 'previous' style = 'display:none;' onclick = "prevPage();">Prev</li>
+			</ul>
+		</nav>
+	</div>
+	<div class="items-per-page-cont pull-right">
+		<label>Jobs Per Page</label>
+		<select class="per-page-val" id = "item_count" onchange = "changeCount()">
+			<option value="10">10</option>
+			<option value="25">25</option>
+			<option value="50">50</option>
+			<option value="100">100</option>
+		</select>
 	</div>
 </div>
+<!--export button-->
+<form id = "exportForm" action = "uploadForm_export.php" method = "POST" enctype="multipart/form-data">
+		<div id="instance-actions">
+			<button id="export-accept">Export</button>
+		</div>
+	</form>
+</div>
+</div>
 
+<div id="internal" class="tab-content">
+<h3>In Process</h3><br>
  <?php
 
 $job_count = 1;
@@ -76,10 +213,10 @@ while($row = $result->fetch_assoc()){
 		$sql100 = "INSERT INTO timestamp (user,time,job, a_p,processed_by,viewed) VALUES ('$user_name', '$today','$job', '$a_p','$processed_by','no')";
 		$result100 = $conn->query($sql100) or die('Error querying database 101.');
 	}
-	
+
 	$job_count = $job_count + 1;
-} 
- 
+}
+
 $result = mysqli_query($conn,"SELECT * FROM job_ticket WHERE processed_by = ''");
 
 
@@ -102,7 +239,7 @@ if ($result->num_rows > 0) {
 		$result1 = mysqli_query($conn, $sql);
 		while($row1 = $result1->fetch_assoc()){
 			echo "<option value = '" . $row1['user'] . "'>" . $row1['first_name'] . ' ' .  $row1['last_name'] . "</option>";
-					
+
 		}
 		echo "</select></form>";
 		echo "</td><td>" .  $row["client_name"]."</td><td>". $row["project_name"]. "</td><td>". $row["due_date"] . "</td><td>". $row["estimate_number"]. "</td><td>". $row["job_status"]. "</td></tr>";
@@ -114,35 +251,18 @@ if ($result->num_rows > 0) {
 }
 ?>
 
+
 </div>
 </div>
 </div>
 </div>
 <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 <script src="sorttable.js"></script>
-<script type="text/javascript" src="jquery-latest.js"></script> 
-<script type="text/javascript" src="jquery.tablesorter.js"></script> 
+<script type="text/javascript" src="jquery-latest.js"></script>
+<script type="text/javascript" src="jquery.tablesorter.js"></script>
 <script>
 
 var fieldCount = 1;
-
-$("#Jobsearch").keyup(function(){
-        _this = this;
-        // Show only matching TR, hide rest of them
-        $.each($("#client_table tbody tr"), function() {
-            if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
-               $(this).hide();
-            else
-               $(this).show();                
-        });
-    }); 
-$(document).ready(function() 
-    { 
-        $("#client_table").tablesorter(); 
-		pageCreator();
-    } 
-);
-
 function showSavedSearch(){
 	if(document.getElementById('show_saved_search').innerHTML == "Show Saved Search"){
 		document.getElementById('saved_search_table').style.display = "block";
@@ -156,9 +276,8 @@ function showSavedSearch(){
 
 function addField(){
 	     if(fieldCount <= 3){
-			$(".advanced_search_area").append("<div class = 'field" + fieldCount + "'><img src = 'images/x_button.png' width = '25' height = '25' onclick = removeField('.field" + fieldCount + "')><input style = 'margin-bottom: 4%' name = 'fieldArea" + fieldCount + "' type = 'text' placeholder = 'Find'>Where<select style = 'width: 125px; font-size: 13px' name = 'select" + fieldCount 
-			+ "'><option selected = 'selected' value = 'full_name'>Client Name</option><option value = 'business'>Business</option><option value = 'address_line_1'>Address</option><option value = 'city'>City</option><option value = 'state'>State</option><option value = 'zipcode'>Zipcode</option><option value = 'call_back_date'>Call back date</option><option value = 'priority'>Priority</option><option value = 'title'>Title</option>" + 
-			"<option value = 'phone'>Phone</option><option value = 'web_address'>Website</option><option value = 'email1'>Email</option><option value = 'vertical1'>Vertical1</option><option value = 'vertical2'>Vertical2</option><option value = 'vertical3'>Vertical3</option></select></div>");
+			$(".advanced_search_area").append("<div class = 'field" + fieldCount + "'><img src = 'images/x_button.png' width = '25' height = '25' onclick = removeField('.field" + fieldCount + "')><input style = 'margin-bottom: 4%' name = 'fieldArea" + fieldCount + "' type = 'text' placeholder = 'Find'>Where<select style = 'width: 125px; font-size: 13px' name = 'select" + fieldCount
+			+ "'><option selected = 'selected' value = 'full_name'>Client Name</option><option value = 'business'>Business</option><option value = 'address_line_1'>Address</option><option value = 'city'>City</option><option value = 'state'>State</option><option value = 'zipcode'>Zipcode</option><option value = 'call_back_date'>Call Back Date</option><option value = 'priority'>Priority</option><option value = 'title'>Title</option>" +"<option value = 'phone'>Phone</option><option value = 'web_address'>Website</option><option value = 'email1'>Email</option><option value = 'vertical1'>Vertical1</option><option value = 'vertical2'>Vertical2</option><option value = 'vertical3'>Vertical3</option></select></div>");
 			if(fieldCount == 1){
 				document.getElementById("advanced_search_button").innerHTML = "Add Field";
 				document.getElementById("advanced_search_submit").style.display = "inline";
@@ -166,7 +285,7 @@ function addField(){
 				document.getElementById("advanced_search_name").style.display = "inline";
 				document.getElementById("advanced_save").style.display = "inline";
 			}
-			
+
 			fieldCount++;
 		 }
 }
@@ -213,16 +332,16 @@ $("#search").keyup(function(){
             if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
                $(this).hide();
             else
-               $(this).show();                
+               $(this).show();
         });
-    }); 
-	
-$(document).ready(function() 
-    { 
-        $("#crm_table").tablesorter(); 
+    });
+
+$(document).ready(function()
+    {
+        $("#crm_table").tablesorter();
 		pageCreator();
-    } 
-); 
+    }
+);
 function pageCreator(){
 	$('table.table-striped.main-table.contacts-list').each(function() {
 		var currentPage = 0;
@@ -252,7 +371,7 @@ function pageCreator(){
 	});
 	document.getElementById("id1").className = "current clickable";
 	var ul = document.getElementById("pag");
-	
+
 	if(ul.childNodes.length > 5){
 		for(var i = 6; i < ul.childNodes.length - 1; i++){
 			ul.children[i].style.display = "none";
@@ -279,7 +398,7 @@ function prevPage(){
 			break;
 		}
 	}
-	
+
 	var newDisplayIndex = 0;
 	for(var i = oldDisplayIndex; i >= 1; i--){
 		newDisplayIndex = i;
@@ -290,7 +409,7 @@ function prevPage(){
 			ul.children[i].style.display = "none";
 		}
 	}
-	
+
 	var count = 0;
 	var lastIndex = 0;
 	for(var i = newDisplayIndex; i >= 1; i--){
@@ -311,7 +430,7 @@ function prevPage(){
 function nextPage(){
 	document.getElementById("prev_button").style.display = "inline";
 	var ul = document.getElementById("pag");
-	
+
 	var displayCount = 0;
 	var lastIndexCheck = 0;
 	for(var i = 1; i < ul.childNodes.length - 1; i++){
@@ -331,7 +450,7 @@ function nextPage(){
 				break;
 			}
 		}
-		
+
 		var newDisplayIndex = 0;
 		for(var i = oldDisplayIndex; i < ul.childNodes.length - 1; i++){
 			if(ul.children[i].style.display == "none"){
@@ -342,7 +461,7 @@ function nextPage(){
 				ul.children[i].style.display = "none";
 			}
 		}
-		
+
 		var count = 0;
 		var lastIndex = 0;
 		for(var i = newDisplayIndex; i < (ul.childNodes.length - subtractValue); i++){
